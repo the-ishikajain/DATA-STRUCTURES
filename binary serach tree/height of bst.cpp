ce159d9327b54d5453f8a8e410ced87d197cf513
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2021-08-18 00:01:52 +0530

    Update height of bst.cpp

--- binary serach tree/height of bst.cpp
+++ binary serach tree/height of bst.cpp
@@ -1,1 +1,58 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+	int data;
+	node* left;
+	node* right;
+};
+node* insert(node*,int);
+node* create(node*);
+int height(node*);
+node* create(int data)
+{
+	node* temp=new node();
+	temp->data=data;
+	temp->left=NULL;
+	temp->right=NULL;
+	return temp;
+}
+node* insert(node* root , int data)
+{
+	if(root==NULL)
+	{
+		root=create(data);
+	}
+	else if(root->data>data)
+	{
+			root->left=insert(root->left,data);
+	}
+		else if(root->data<data)
+	{
+			root->right=insert(root->right,data);
+	}
+	return root;
+}
+int height(node* root)
+{
+	if(root==NULL)
+	return -1;
+	int lh=height(root->left);
+	int rh=height(root->right);
+	return max(lh,rh)+1;
+}
+int main()
+{
+	node* root=NULL;
+	root=insert(root,12);
+	insert(root,10);
+	insert(root,30);
+	insert(root,15);
+	insert(root,45);
+	insert(root,9);
+	insert(root,1);
+	insert(root,5);
+	insert(root,4);
+	int h=height(root);
+	cout<<"HEIGHT OF BST IS = "<<h;
+}
