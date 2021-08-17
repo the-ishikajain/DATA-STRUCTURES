2abefc2216442088a7ef37a6152e41100789a0a8
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2021-08-18 00:15:21 +0530

    Create deletion at any position.cpp

---
+++ Linked List/Singly linked list/deletion at any position.cpp
@@ -0,0 +1,82 @@
+//linked list deletion at any position
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+	int data;
+	node* link;
+};
+void print();
+void insert(int data);
+void delete1(int k);
+node* head;
+int main()
+{
+	head=NULL;
+	int n;
+	cout<<"ENTER NUMBER OF ELEMENTS YOU WANT TO INSERT: "<<endl;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		int x;
+		cout<<endl<<"ENTER ELEMENT"<<endl;
+		cin>>x;
+		insert(x);
+		print();
+	}
+	int k;
+	cout<<endl<<"ENTER POSITION YOU WANT TO DELETE=";
+	cin>>k;
+	delete1(k);
+	print();
+	return 0;
+}
+void insert(int data)
+{
+	node* temp=new node();
+	temp->data=data;
+	temp->link=NULL;
+	if(head==NULL)
+	{
+		head=temp;
+	}
+	else
+	{
+		node* temp1=new node();
+	    temp1=head;
+	while((temp1->link)!=NULL)
+	{
+		temp1=(temp1->link);
+	}
+	(temp1->link)=temp;
+	}
+}
+void print()
+{
+	node* temp=head;
+	cout<<"list is:"<<endl;
+	while(temp!=NULL)
+	{
+		cout<<(temp->data)<<" ";
+		temp=(temp->link);
+	}
+}
+void delete1(int k)
+{
+	node* temp=head;
+	if(k==0)
+	{
+	head=temp->link;
+	free(temp);	
+	}
+	else
+	{
+	for(int i=0;i<k-1;i++)
+	{
+		temp=temp->link;
+	}
+	node* temp1=temp->link;
+	temp->link=temp1->link;
+	free(temp1);
+    }  
+}
